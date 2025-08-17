bc85e7b68d50ad66d7730e272c8d2da23f422ea4
Ndifreke Ndue <ndifrekeh@gmail.com>
2025-08-17 18:31:45 +0100

    Modified script.js to implement 'Task 1' - Implementing Web Storage and JSON Handling

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -93,6 +93,17 @@ document.addEventListener("DOMContentLoaded", function() {
         })
     }
 
+    function importFromJsonFile(event) {
+        const fileReader = new FileReader();
+        fileReader.onload = function(event) {
+        const importedQuotes = JSON.parse(event.target.result);
+        quotes.push(...importedQuotes);
+        saveQuotes();
+        alert('Quotes imported successfully!');
+    };
+        fileReader.readAsText(event.target.files[0]);
+  }
+
     // const addQuoteButton = document.getElementById("addQuoteButton");
     // addQuoteButton.addEventListener("click", createAddQuoteForm);
 
